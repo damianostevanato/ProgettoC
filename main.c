272f3b994f7dbd944958bc194845333fca818f67
Avets <da.stevanato@gmail.com>
2020-05-05 20:09:07 +0200

    changed main

--- main.c
+++ main.c
@@ -5,20 +5,13 @@
 #include "ip_lib.h"
 
 int main(){
-    ip_mat *mat,*mat2,*concat,*somma;
+    ip_mat *mat,*mat2,*concat;
     mat=ip_mat_create(4,4,3,1.0);
-    mat2=ip_mat_create(4,4,3,2.0);
-    concat=ip_mat_concat(mat,mat2,0);
-    somma = ip_mat_sum(mat,mat2);
-    ip_mat_show(mat);
-    printf("----------------");
-    ip_mat_show(mat2);
-    printf("----------------");
-    ip_mat_show(somma);
-    printf("----------------");
+    mat2=ip_mat_create(4,4,1,2.0);
+    concat=ip_mat_concat(mat,mat2,2);
+    ip_mat_show(concat);
     ip_mat_free(mat);
     ip_mat_free(mat2);
-    ip_mat_free(somma);
     ip_mat_free(concat);
     return 0;
 }
