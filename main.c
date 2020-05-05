09e6e81aff91e9b3ce8c1706da2481e76f7dc7d3
osammot10 <tommasopellizzon2000@gmail.com>
2020-05-05 21:41:24 +0200

    added mean

--- main.c
+++ main.c
@@ -14,18 +14,18 @@ gcc ip_lib.o bmp.o main.c -o main -lm -Wall -g -O1
 valgrind -v --leak-check=full ./main
 */
 int main(){
-    ip_mat *mat,*mat2,*concat,*scalare,*s_scalare;
+    ip_mat *mat,*mat2,*concat;
     mat=ip_mat_create(4,4,3,1.0);
     mat2=ip_mat_create(4,4,3,2.0);
-    concat=ip_mat_sub(mat,mat2);
-    scalare = ip_mat_mul_scalar(mat,5);
-    s_scalare = ip_mat_add_scalar(mat,5);
-    ip_mat_show(scalare);
-    ip_mat_show(s_scalare);
+    concat=ip_mat_mean(mat,mat2);
+    //scalare = ip_mat_mul_scalar(mat,5);
+    //s_scalare = ip_mat_add_scalar(mat,5);
+    ip_mat_show(concat);
+    //ip_mat_show(s_scalare);
     ip_mat_free(mat);
-    ip_mat_free(s_scalare);
+    //ip_mat_free(s_scalare);
     ip_mat_free(mat2);
     ip_mat_free(concat);
-    ip_mat_free(scalare);
+    //ip_mat_free(scalare);
     return 0;
 }
